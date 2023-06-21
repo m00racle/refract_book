45c3952e15d5b5e5f044be8b3dae169e056a7a17
mooracle <yanuar.prakosa@gmail.com>
2023-06-21 18:37:42 +0700

    trial with default components from the material UI

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import ProTip from '../src/ProTip';
 import Box from '@mui/material/Box';
-
-import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
 export default function Home() {
   return (
     <Box>
         <Typography variant='h1' component='p'>Let's gone with h1</Typography>
+        <Container>
+            <ProTip />
+        </Container>
         <Copyright />
     </Box>
   );
