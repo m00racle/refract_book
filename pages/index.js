68d0138611cf929d08c2cc72b76345941b715e5c
mooracle <yanuar.prakosa@gmail.com>
2023-06-21 18:29:36 +0700

    first trial material ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-function HomePage() {
-  return <h1>Hello, Next.js!</h1>;
+import Link from '../src/Link';
+import Copyright from '../src/Copyright';
+
+export default function Home() {
+  return (
+    <Box>
+        <Typography variant='h1' component='p'>Let's gone with h1</Typography>
+        <Copyright />
+    </Box>
+  );
 }
-
-export default HomePage;
