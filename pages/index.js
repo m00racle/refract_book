7eb0f8b4d1a77c349ce817f1a1fa5992a7a0d375
mooracle <yanuar.prakosa@gmail.com>
2023-06-21 20:57:06 +0700

    fix landing page looks

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import ProTip from '../src/ProTip';
-import Box from '@mui/material/Box';
-import Copyright from '../src/Copyright';
 import Head from 'next/head';
 import { Button } from '@mui/material';
 import styles from '../styles/landing.module.scss';
@@ -12,13 +9,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Expense Tracker</title>
+        <title>Bookeeper</title>
       </Head>
 
       <main>
         <Container className={styles.container}>
-          <Typography variant="h1">Welcome to Expense Tracker!</Typography>
-          <Typography variant="h2">Add, view, edit, and delete expenses</Typography>
+          <Typography variant="h1">Welcome to Bookeeper!</Typography>
+          <Typography variant="h2">Simple Accounting App</Typography>
           <div className={styles.buttons}>
             <Button variant="contained" color="secondary">
               Login / Register
