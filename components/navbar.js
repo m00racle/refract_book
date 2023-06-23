125ae090522fb2544e8fc34fad369383ecc5a839
mooracle <yanuar.prakosa@gmail.com>
2023-06-23 08:28:24 +0700

    simplify nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -7,20 +7,11 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function ButtonNavBar() {
+export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             user@gmail.com
           </Typography>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Typography, Button } from '@mui/material';
 import styles from '../styles/dashboard.module.scss';
 import Head from 'next/head';
-import ButtonNavBar from '../components/navbar';
+import NavBar from '../components/navbar';
 
 const Dashboard = ({ userEmail, onSignOut }) => {
   return (
     <div>
       <Head><title>Bookeeper</title></Head>
 
-      <ButtonNavBar />
+      <NavBar />
       <div>
         <Button variant='contained' color='secondary'>Book</Button>
       </div>
