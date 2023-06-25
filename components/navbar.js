8b5daaeb78d81339220bf6441efa8388062ce48a
mooracle <yanuar.prakosa@gmail.com>
2023-06-25 12:37:53 +0700

    set the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -7,15 +7,24 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function NavBar() {
+export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 2 }}>
             user@gmail.com
           </Typography>
-          <Button color="inherit">Sign Out</Button>
+          <Button variant='contained' color='secondary'>Sign Out</Button>
         </Toolbar>
       </AppBar>
     </Box>
