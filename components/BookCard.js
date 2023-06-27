0d782a8fa36031a7dbdacdaf1ed266cd1c19a4fb
mooracle <yanuar.prakosa@gmail.com>
2023-06-27 20:12:18 +0700

    change edit and delete button from box to stack

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,6 +1,6 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import Fab from '@mui/material/Fab';
@@ -11,12 +11,12 @@ export default function BookCard() {
             <Paper elevation={3} >
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant='subtitle' component='h2' marginLeft={"1em"}>Book1</Typography>
-                    <Box>
+                    <Stack direction="row" spacing={"0.5em"}>
                         
-                        <Fab color='primary' aria-label='edit book' sx={{margin: "1em"}}><ModeEditOutlineIcon /></Fab>
+                        <Fab color='primary' aria-label='edit book' ><ModeEditOutlineIcon /></Fab>
                         
-                        <Fab color='secondary' aria-label='delete book' sx={{margin: "1em"}}><DeleteForeverIcon /></Fab>
-                    </Box>
+                        <Fab color='secondary' aria-label='delete book' ><DeleteForeverIcon /></Fab>
+                    </Stack>
                 </Box>
             </Paper>
         </Grid>
