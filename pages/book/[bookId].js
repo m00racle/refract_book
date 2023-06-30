098ee13e7afb32c864a56f2a808b9c9ad281747e
mooracle <yanuar.prakosa@gmail.com>
2023-06-30 08:55:50 +0700

    put additional componenet and head template to book page

--- pages/book/[bookId].js
+++ pages/book/[bookId].js
@@ -1,12 +1,19 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import NavBar from '../../components/navbar';
 
 export default function BookPage() {
   const router = useRouter();
   const { bookId } = router.query;
 
   return (
+    <>
+    <Head><title>Book {bookId}</title></Head>
+
+    <NavBar />
     <div>
       <h1>Book ID: {bookId}</h1>
     </div>
+    </>
   );
 }
