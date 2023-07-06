0b2b9180481d2dda696697ee135f3b1f0eafedd9
mooracle <yanuar.prakosa@gmail.com>
2023-07-06 17:16:47 +0700

    sign in with google works

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
-import * as React from 'react';
+
+import { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Head from 'next/head';
-import { Button } from '@mui/material';
+import { Button, Dialog } from '@mui/material';
 import styles from '../styles/landing.module.scss';
+import { useRouter } from 'next/router';
+import { auth } from '../firebase/firebase';
+import { useAuth } from '../firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { GoogleAuthProvider } from 'firebase/auth';
+
+// when user logged in redirect to dashboard:
+const REDIRECT_PAGE = '/dashboard';
+
+// config for Firebase UI auth
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: REDIRECT_PAGE,
+  signInOptions: [
+    GoogleAuthProvider.PROVIDER_ID
+  ],
+};
 
 export default function Home() {
+  // what is the auth state? is it still loading?
+  const { authUser, isLoading } = useAuth();
+
+  const router = useRouter();
+  
+  // make state for login page from StyledFirebaseAuth
+  const [login, setLogin] = useState(false);
+
+  // when user is authenticated then authUser !== null and isLoading is false:
+  // then redirect them to dashboard
+  useEffect(() => {
+    if (!isLoading && authUser) {
+      router.push(REDIRECT_PAGE);
+    }
+  }, [authUser, isLoading]);
+
   return (
     <div>
       <Head>
@@ -17,10 +51,14 @@ export default function Home() {
           <Typography variant="h1">Welcome to Bookeeper!</Typography>
           <Typography variant="h2">Simple Accounting App</Typography>
           <div className={styles.buttons}>
-            <Button variant="contained" color="secondary">
+            <Button variant="contained" color="secondary" onClick={() => setLogin(true)}>
+              {/* button clicked will set login = true which open dialog */}
               Login / Register
             </Button>
           </div>
+          <Dialog onClose={() => setLogin(false)} open={login}>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}/>
+          </Dialog>
         </Container>
       </main>
     </div>);
