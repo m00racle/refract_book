adf4f794c3d30f519275d316973b6170b1749b1a
mooracle <yanuar.prakosa@gmail.com>
2023-09-30 08:07:34 +0700

    rename firebase test to __fire_tests__

---
+++ __fire_tests__/firestore-rules.test.js
@@ -0,0 +1,33 @@
+import { 
+    assertFails,
+    assertSucceeds,
+    initializeTestEnvironment,
+    RulesTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import { readFileSync } from "node:fs";
+import { doc, getDoc, addDoc, collection } from "firebase/firestore";
+
+//  const MY_PROJECT_ID = "refract-book";
+
+ describe("firestore-book rules", () => {
+    test("Understand basic addition", () => {
+        expect(2 + 2).toBe(4);
+    });
+
+    test("Unauthorized user must not add doc", async() => {
+        // set the test environment
+        let testEnv = await initializeTestEnvironment({
+            projectId: "refract-book",
+            firestore: {
+                host: "127.0.0.1",
+                port: 8080,
+                rules: readFileSync("firestore.rules","utf8")
+            },
+        });
+
+        // make unauth context
+        let unauthDb = testEnv.unauthenticatedContext().firestore();
+        await assertFails(addDoc(collection(unauthDb, "books"),{name: "book1"}));
+    });
+    
+ });
