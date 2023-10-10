d3a674d48cdc6ad88c7b1e615ac9dd0abec2aea7
mooracle <yanuar.prakosa@gmail.com>
2023-10-10 21:51:39 +0700

    add test scenarios for set accounts

--- __fire_tests__/firestore-account.test.js
+++ __fire_tests__/firestore-account.test.js
@@ -5,10 +5,17 @@
 */
 
 import { initializeTestEnvironment, assertFails, assertSucceeds } from "@firebase/rules-unit-testing";
-import { setLogLevel } from "firebase/firestore";
+import { addDoc, collection, doc, setDoc, setLogLevel } from "firebase/firestore";
 import { readFileSync } from "node:fs";
 
 let testEnv;
+const sampleAccountAlice = {
+    id: "11001",
+    refs: {
+        book_ref: "alice-book1"
+    },
+    account_name: "cash",
+};
 
 beforeAll(async() => {
     /*  
@@ -46,25 +53,51 @@ beforeEach(async () => {
         ensure you arrage (prepare) the data and don't use previous used data
     */
     await testEnv.clearFirestore();
+    // preps a book which will have accounts collection
+    await testEnv.withSecurityRulesDisabled(async (context) => {
+        await setDoc(doc(context.firestore(), "books", "alice-book1"), {
+            id: "alice-book1",
+            refs: {
+                user_id: "alice",
+                book_ref: 3
+            },
+            name: "Book sample3"
+        });
+    });
 });
 
 describe("firestore-account rules", () => {
     /* 
         this part is exclusive to test the firestore.rules 
     */
-    test("unauth user must not add accounts", async () => {
+    test("unauth user must not set accounts", async () => {
         /*  
             test unauthorized user must not add accounts
             this will use the firestore.rules for /books/{bookId}/accounts/ path
-            then it will use addDoc and the test MUST assert to fail
+            then it will use setDoc and the test MUST assert to fail
+            NOTE: setDoc because we want to specify the account id as standard accounting id
         */
-        // expect(true).toBeFalsy();
+        let unauthDb = testEnv.unauthenticatedContext().firestore();
+        // act: setDoc while unauthenticated
+        await assertFails(setDoc(doc(unauthDb,"books/alice-book1/accounts", "11001"), sampleAccountAlice));
     });
 
-    test("auth user can add accounts", async () => {
+    test("auth user can set accounts", async () => {
         /*  
-            test authorized user must be able to add accounts
+            test authorized user must be able to set accounts
+            set accounts with id "11001"
         */
+        // set auth with uid alice
+        let authDb = testEnv.authenticatedContext('alice').firestore();
+        await assertSucceeds(setDoc(doc(authDb, "books/alice-book1/accounts", "11001"), sampleAccountAlice));
+    });
+
+    test("auth user with different uid can't set accounts", async () => {
+        /* 
+            test case when a user is authenticated but have different uid can't set accounts
+        */
+        let authDb = testEnv.authenticatedContext('bruce').firestore();
+        await assertFails(setDoc(doc(authDb, "books/alice-book1/accounts", "11001"), sampleAccountAlice));
     });
 
     test("only able to get account from valid book", async () => {
