76c234b7b1cd94cc491c048f9590cf4eaa8bd12f
mooracle <yanuar.prakosa@gmail.com>
2023-11-21 14:53:38 +0700

    add books for alice, bruce, chase for testing

--- __fire_tests__/fire-account-rules.test.js
+++ __fire_tests__/fire-account-rules.test.js
@@ -17,6 +17,7 @@ import {
 let testEnv3; // name of the test environment
 let aliceDb, bruceDb, chaseDb; // 3 database represent 3 types of user
 let addRef; // reference as the result of addAccount
+let initDocs; // initial data for books
 
 beforeAll(async() => {
     /* 
@@ -41,7 +42,65 @@ beforeAll(async() => {
     bruceDb = testEnv3.authenticatedContext('bruce').firestore();
     chaseDb = testEnv3.unauthenticatedContext().firestore();
 
-    // TODO: create books for the prepared scenarios
+    // : create books for the prepared scenarios
+    initDocs = [
+        {
+            refs: {
+                user_id: 'alice'
+            },
+            id: "alice-book-1",
+            name: "sample book alice 1",
+            initial: "SBA1",
+            email: "alice@example.com",
+            business_type: "perorangan",
+            npwp: "",
+            logoUrl: ""
+        },
+        {
+            refs: {
+                user_id: 'alice'
+            },
+            id: "alice-book-2",
+            name: "sample book alice 2",
+            initial: "SBA2",
+            email: "alice2@example.com",
+            business_type: "firma",
+            npwp: "123456-7890",
+            logoUrl: ""
+        },
+        {
+            refs: {
+                user_id: 'bruce'
+            },
+            id: "bruce-book-1",
+            name: "sample book Bruce 1",
+            initial: "SBB1",
+            email: "bruce@example.com",
+            business_type: "komanditer",
+            npwp: "09876545",
+            logoUrl: ""
+        },
+        {
+            refs: {
+                user_id: 'chase'
+            },
+            id: "chase-book-1",
+            name: "sample book CHASE 1",
+            initial: "SBC1",
+            email: "chase@example.com",
+            business_type: "perorangan",
+            npwp: "",
+            logoUrl: ""
+        }
+    ];
+
+    // load the initData
+    await testEnv3.withSecurityRulesDisabled(async (context) => {
+        const initDb = context.firestore();
+        for (const initDoc of initDocs) {
+            await setDoc(doc(initDb, "books", initDoc.id), initDoc);
+        }
+    });
 });
 
 afterAll(async() => {
